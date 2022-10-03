41498f39ef6a329fc84e377a5cf163c814e0b7f0
mditaranto <mditaranto@106-04.info2>
2022-10-03 10:09:43 +0200

    prueba1

---
+++ Pruebagit/src/prueba/Prueba.java
@@ -0,0 +1,17 @@
+package prueba;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		//Definimos variables
+		final float Kperas;
+		final float Kmanz;
+		int Vperas;
+		int Vmanz;
+		float total;
+		Kperas = 1.95f;
+		Kmanz = 2.35f;
+
+	}
+
+}
